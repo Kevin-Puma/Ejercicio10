68f434a27ed340896903be67eae7d80f0d9359eb
User <User@DESKTOP-CRJD420>
2023-08-28 23:59:31 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Ingrese el consumo total: ");
+decimal consumo = Convert.ToDecimal(Console.ReadLine());
+
+decimal descuento = 0;
+if (consumo <= 100)
+{
+    descuento = consumo * 0.10m;
+}
+else
+{
+    descuento = consumo * 0.20m;
+}
+
+decimal subtotal = consumo - descuento;
+
+decimal impuesto = subtotal * 0.18m;
+
+decimal totalAPagar = subtotal + impuesto;
+
+Console.WriteLine("Consumo: S/" +consumo+ "");
+Console.WriteLine("Descuento: S/" +descuento+ "");
+Console.WriteLine("Impuesto (18%): S/" + impuesto + "");
+Console.WriteLine("Subtotal (sin impuesto): S/" +subtotal+ "");
+Console.WriteLine("Total a pagar: S/" +totalAPagar+ "");
